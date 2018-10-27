767c2a17b9c6a3cdef2412e0af0e16b949065e76
Becky <becky@monometric.net>
2018-10-27 13:20:29 +0200

    Add game board with drag input

---
+++ src/DraggingOverlay.js
@@ -0,0 +1,70 @@
+import React, {useRef, useState} from 'react';
+
+function getRelativePosition(event, ref) {
+    const {x: offsetX, y: offsetY} = ref.current.getBoundingClientRect();
+    const {pageX, pageY} = event;
+    const {offsetWidth, offsetHeight} = ref.current;
+    const x = (pageX - offsetX) / offsetWidth;
+    const y = (pageY - offsetY) / offsetHeight;
+
+    return {x, y};
+}
+
+export function DraggingOverlay(props) {
+    const ref = useRef();
+    const [pointerDown, setPointerDown] = useState(false);
+
+    const style = {
+        height: '100%',
+        position: 'absolute',
+        width: '100%',
+        zIndex: props.zIndex === undefined ? 10000 : props.zIndex,
+    };
+
+    function startDragging(event) {
+        setPointerDown(true);
+        props.onDragStart(getRelativePosition(event, ref));
+    }
+
+    function finishDragging(event) {
+        if (!pointerDown) {
+            return;
+        }
+
+        setPointerDown(false);
+        props.onDragEnd(getRelativePosition(event, ref));
+    }
+
+    function abortDragging() {
+        if (!pointerDown) {
+            return;
+        }
+
+        setPointerDown(false);
+        props.onDragAbort();
+    }
+
+    function updatePosition(event) {
+        if (!pointerDown) {
+            return;
+        }
+
+        props.onDragMove(getRelativePosition(event, ref));
+    }
+
+    return (
+        <div
+            style={style}
+            touch-action="none"
+            onPointerDown={(event) => startDragging(event) }
+            onPointerUp={(event) => finishDragging(event) }
+            onPointerMove={(event) => updatePosition(event)}
+            onPointerCancel={() => abortDragging()}
+            onPointerLeave={() => abortDragging()}
+            onPointerOut={() => abortDragging()}
+            onLostPointerCapture={() => abortDragging()}
+            ref={ref}
+        >
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,92 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {DraggingOverlay} from './DraggingOverlay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const BOARD_SIZE = 7;
+
+function Square(props) {
+    const classes = [
+        'board__square',
+        'board__square--animal-' + props.value,
+    ];
+
+    if (props.active) {
+        classes.push('board__square--active');
+    }
+
+    return (
+        <div className={classes.join(' ')}>
+        </div>
+    );
+}
+
+function Board(props) {
+    const [dragStart, setDragStart] = useState(null);
+    const [activeSquares, setActiveSquares] = useState([]);
+    const [board, setBoard] = useState([...Array(7 * 7)].map(() => Math.random() * 3 | 0));
+
+    const squares = board.map((value, index) => {
+        return (
+            <Square
+                key={index}
+                value={value}
+                active={activeSquares.includes(index)}
+            />
+        );
+    });
+
+    function getActiveSquares(start, end) {
+        if (board[start] !== board[end]) {
+            return [start];
+        }
+
+        return [start, end];
+    }
+
+    function getIndex(x, y) {
+        return (x * BOARD_SIZE | 0) + (y * BOARD_SIZE | 0) * 7;
+    }
+
+    function handleDragStart({x, y}) {
+        const index = getIndex(x, y);
+        setDragStart(index);
+        setActiveSquares([index]);
+    }
+
+    function handleDragEnd({x, y}) {
+        setDragStart(null);
+        setActiveSquares([]);
+    }
+
+    function handleDragMove({x, y}) {
+        setActiveSquares(getActiveSquares(dragStart, getIndex(x, y)));
+    }
+
+    function handleDragAbort() {
+        setDragStart(null);
+        setActiveSquares([]);
+    }
+
+    return (
+        <div>
+            <div className="board">
+                <DraggingOverlay
+                    onDragStart={handleDragStart}
+                    onDragEnd={handleDragEnd}
+                    onDragMove={handleDragMove}
+                    onDragAbort={handleDragAbort}
+                />
+                <div className="board__board">
+                    {squares}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+ReactDOM.render(<Board />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
