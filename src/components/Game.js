92ed413578b2b0b2e36c10801562af167afa84e2
Becky <becky@monometric.net>
2018-11-04 15:40:33 +0100

    Move selection logic to game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,7 +6,51 @@ import Title from './Title';
 const BOARD_SIZE = 7;
 const COLORS = 3;
 
+function isCoordinateValid(c) {
+    return 0 <= c && c < BOARD_SIZE && c % 1 === 0;
+}
+
+function getSelection(board, diagonal) {
+    if (diagonal === null) {
+        return {squares: [], size: 0};
+    }
+
+    const start = diagonal.start.x + diagonal.start.y * BOARD_SIZE;
+    const end = diagonal.end.x + diagonal.end.y * BOARD_SIZE;
+
+    if (start === end) {
+        return {squares: [start], size: 0};
+    }
+
+    const a = {x: start % BOARD_SIZE, y: start / BOARD_SIZE | 0};
+    const b = {x: end % BOARD_SIZE, y: end / BOARD_SIZE | 0};
+    const center = {x: (a.x + b.x) / 2, y: (a.y + b.y) / 2 };
+    const diff = {x: center.x - a.x, y: center.y - a.y};
+    const selection = {squares: [start], size: 2 * Math.hypot(diff.x, diff.y)};
+
+    if (board[start] === board[end]) {
+        selection.squares.push(end);
+    }
+
+    const c = {x: center.x + diff.y, y: center.y - diff.x};
+    const cIndex = c.x + c.y * BOARD_SIZE;
+
+    if (isCoordinateValid(c.x) && isCoordinateValid(c.y) && board[cIndex] === board[start]) {
+        selection.squares.push(cIndex);
+    }
+
+    const d = {x: center.x - diff.y, y: center.y + diff.x};
+    const dIndex = d.x + d.y * BOARD_SIZE;
+
+    if (isCoordinateValid(d.x) && isCoordinateValid(d.y) && board[dIndex] === board[start]) {
+        selection.squares.push(dIndex);
+    }
+
+    return selection;
+}
+
 export default function Game(props) {
+
     const [getRandomType, getRandomTypes] = useRandomBucket(0, COLORS);
 
     let top = (() => {
@@ -17,6 +61,8 @@ export default function Game(props) {
         return (
             <Board
                 board={props.board}
+                gridSize={BOARD_SIZE}
+                getSelection={(diagonal) => getSelection(props.board, diagonal)}
                 replaceSquares={(indexes, size) => {
                     props.replaceSquares(
                         indexes.map((index) => ({
--- src/components/board.js
+++ src/components/board.js
@@ -2,14 +2,12 @@ import React, {useState} from 'react';
 import delay from 'delay';
 import {DraggingOverlay} from './DraggingOverlay';
 
-const BOARD_SIZE = 7;
-
 export default function Board(props) {
     const [drag, setDrag] = useState(null);
     const [hiddenSquares, setHiddenSquares] = useState([]);
     const [isLocked, setLocked] = useState(false);
 
-    const selection = getSelection(drag);
+    const selection = props.getSelection(drag);
 
     const squares = props.board.map((value, index) => {
         const active = selection.squares.includes(index);
@@ -29,48 +27,6 @@ export default function Board(props) {
             </div>
         );
     });
-
-    function isCoordinateValid(c) {
-        return 0 <= c && c < BOARD_SIZE && c % 1 === 0;
-    }
-
-    function getSelection(drag) {
-        if (drag === null) {
-            return {squares: [], size: 0};
-        }
-
-        const {start, end} = drag;
-
-        if (end === null || start === end) {
-            return {squares: [start], size: 0};
-        }
-
-        const a = {x: start % BOARD_SIZE, y: start / BOARD_SIZE | 0};
-        const b = {x: end % BOARD_SIZE, y: end / BOARD_SIZE | 0};
-        const center = {x: (a.x + b.x) / 2, y: (a.y + b.y) / 2 };
-        const diff = {x: center.x - a.x, y: center.y - a.y};
-        const selection = {squares: [start], size: 2 * Math.hypot(diff.x, diff.y)};
-
-        if (props.board[start] === props.board[end]) {
-            selection.squares.push(end);
-        }
-
-        const c = {x: center.x + diff.y, y: center.y - diff.x};
-        const cIndex = c.x + c.y * BOARD_SIZE;
-
-        if (isCoordinateValid(c.x) && isCoordinateValid(c.y) && props.board[cIndex] === props.board[start]) {
-            selection.squares.push(cIndex);
-        }
-
-        const d = {x: center.x - diff.y, y: center.y + diff.x};
-        const dIndex = d.x + d.y * BOARD_SIZE;
-
-        if (isCoordinateValid(d.x) && isCoordinateValid(d.y) && props.board[dIndex] === props.board[start]) {
-            selection.squares.push(dIndex);
-        }
-
-        return selection;
-    }
 
     async function handleDragEnd() {
         setDrag(null);
@@ -94,15 +50,15 @@ export default function Board(props) {
 
     function handleDragUpdate(start, end) {
         setDrag(start && {
-            start: start.x + start.y * BOARD_SIZE,
-            end: end.x + end.y * BOARD_SIZE,
+            start,
+            end,
         });
     }
 
     return (
         <div block="board">
             <DraggingOverlay
-                gridSize={BOARD_SIZE}
+                gridSize={props.gridSize}
                 onDragEnd={handleDragEnd}
                 onDragUpdate={handleDragUpdate}
                 isLocked={isLocked}
