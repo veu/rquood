5b36405c7152b4edbedb37fb8688c4f4193fa3a5
Becky <becky@monometric.net>
2020-01-26 16:54:25 +0100

    Update

---
+++ precache-manifest.681fe1c7f695e2c8e1bda6f155efc416.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d32c56260fac0e4b8d1",
+    "url": "/static/css/main.7b780a3f.chunk.css"
+  },
+  {
+    "revision": "2d32c56260fac0e4b8d1",
+    "url": "/static/js/main.2d32c562.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "6d825ade67869897385c",
+    "url": "/static/js/2.6d825ade.chunk.js"
+  },
+  {
+    "revision": "d85c7aee9a93c78180c1125bc54e3f60",
+    "url": "/index.html"
+  }
+];
