60b9233825a6db7139efa732087b0bea5ed1cc00
Becky <becky@monometric.net>
2020-02-06 22:45:21 +0100

    Update

---
+++ precache-manifest.533db6b4aa0c6516d422d2bad40d98d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ee77504846333c1aa82",
+    "url": "/static/css/main.8c988ba5.chunk.css"
+  },
+  {
+    "revision": "3ee77504846333c1aa82",
+    "url": "/static/js/main.3ee77504.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "29f5b8cb94ed61522a5e",
+    "url": "/static/js/2.29f5b8cb.chunk.js"
+  },
+  {
+    "revision": "cd144be9616e4cad1df200e1ca319437",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.297547fee3f34cbd9e629d2df9553cdd.js"
+  "/precache-manifest.533db6b4aa0c6516d422d2bad40d98d8.js"
 );
 
 workbox.clientsClaim();
