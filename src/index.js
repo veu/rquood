1269adeb832463c541e8d7107bd730624ba62241
Becky <becky@monometric.net>
2020-02-16 14:07:26 +0100

    Install normalize and update dependencies

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import persistState from 'redux-localstorage';
 import { Switch, Route } from 'react-router';
 import { routerMiddleware, ConnectedRouter } from 'connected-react-router';
 import { createHashHistory } from 'history';
+import 'normalize.css';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import createReducers from './state/reducers';
